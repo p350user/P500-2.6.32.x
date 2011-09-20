150db46d0ff8564888492694ff580c9bc44aa38a
Gérson Barreiros <fserve@gmail.com>
2011-09-20 00:51:13 -0400

    lib: Introduce some memory copy macros and functions
    	lib/memcopy.c

--- include/linux/memcopy.h
+++ include/linux/memcopy.h
@@ -3,7 +3,11 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
+<<<<<<< HEAD
  * Software Foundation; either version 2.1 of the License, or (at your option)
+=======
+ * Software Foundation; either version 2 of the License, or (at your option)
+>>>>>>> 8b61c2a... lib: Introduce some memory copy macros and functions
  * any later version.
  * 
  * This program is distributed in the hope that it will be useful,
@@ -13,7 +17,11 @@
  * 
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
+<<<<<<< HEAD
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+=======
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+>>>>>>> 8b61c2a... lib: Introduce some memory copy macros and functions
  *
  * The code is derived from the GNU C Library.
  * Copyright (C) 1991, 1992, 1993, 1997, 2004 Free Software Foundation, Inc.
--- lib/memcopy.c
+++ lib/memcopy.c
@@ -3,7 +3,11 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
+<<<<<<< HEAD
  * Software Foundation; either version 2.1 of the License, or (at your option)
+=======
+ * Software Foundation; either version 2 of the License, or (at your option)
+>>>>>>> 8b61c2a... lib: Introduce some memory copy macros and functions
  * any later version.
  * 
  * This program is distributed in the hope that it will be useful,
@@ -13,7 +17,11 @@
  * 
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
+<<<<<<< HEAD
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+=======
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+>>>>>>> 8b61c2a... lib: Introduce some memory copy macros and functions
  *
  * The code is derived from the GNU C Library.
  * Copyright (C) 1991, 1992, 1993, 1997, 2004 Free Software Foundation, Inc.
@@ -400,4 +408,7 @@ do1:
 do0:
 	((op_t *) dstp)[3] = MERGE (a0, sh_1, a1, sh_2);
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8b61c2a... lib: Introduce some memory copy macros and functions
