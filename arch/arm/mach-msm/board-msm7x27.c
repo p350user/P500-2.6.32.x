803f21b8072b9be68d4d88f6caf9148592037576
Gérson Barreiros <fserve@gmail.com>
2011-11-15 13:43:50 -0400

    Fix for audio making weird noises while at low clocks. (Modified size of MSM_PMEM_AUDIO_SIZE and PMEM_KERNEL_EBI1_SIZE.)

--- arch/arm/mach-msm/board-msm7x27.c
+++ arch/arm/mach-msm/board-msm7x27.c
@@ -85,10 +85,12 @@
 #define MSM_PMEM_MDP_SIZE  	0x1700000 /*msm: 7x27: move video encoder to /dev/pmem_adsp*/
 //#define MSM_PMEM_MDP_SIZE	0x1B76000
 #define MSM_PMEM_ADSP_SIZE	0xB71000
-#define MSM_PMEM_AUDIO_SIZE	0x5B000
+#define MSM_PMEM_AUDIO_SIZE	0x121000
+//#define MSM_PMEM_AUDIO_SIZE	0x5B000
 #define MSM_FB_SIZE		0x177000
 #define MSM_GPU_PHYS_SIZE	SZ_2M
-#define PMEM_KERNEL_EBI1_SIZE	0x1C000
+#define PMEM_KERNEL_EBI1_SIZE	0x64000
+//#define PMEM_KERNEL_EBI1_SIZE	0x1C000
 /* Using lower 1MB of OEMSBL memory for GPU_PHYS */
 #define MSM_GPU_PHYS_START_ADDR	 0xD600000ul
 #endif
